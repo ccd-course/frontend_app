19c868ea237da4827e361d81bafdb9f17ae1acb5
majd-adawieh <majd.adawieh@icloud.com>
2021-12-05 16:09:37 +0100

    Add page not found route and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
+import { PageNotFound } from "./pages/PageNotFound";
 
 export const App = () => {
   return (
     <div className="App" style={{ display: "flex", flexDirection: "column" }}>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+export const PageNotFound = () => {
+  return (
+    <div>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <div style={{ display: "inline" }}>Page Not Found</div>
+      </Typography>
+    </div>
+  );
+};
