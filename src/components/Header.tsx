9494b3093a16d6c1ad5a1e395bacc930fc674a54
majd-adawieh <majd.adawieh@icloud.com>
2021-12-05 16:13:28 +0100

    Remove unneeded imports in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
 
 export const Header = (props: any) => {
   return (
