dc890df3703b094aa0d237e9ed8c155e8cb0e475
majd-adawieh <majd.adawieh@icloud.com>
2021-12-05 16:36:43 +0100

    add a button to start the game

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,6 +1,33 @@
+import { Button } from "@mui/material";
 import React from "react";
+import { StartGameButton } from "../styles/ButtonStyles";
 import { PageStyle } from "../styles/DefaultPagesStyle";
 
 export const LandingPage = () => {
-  return <div style={PageStyle}>Landing Page</div>;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  return (
+    <div style={PageStyle}>
+      <div
+        style={{
+          position: "absolute",
+          top: "80%",
+          left: "50%",
+          transform: "translate(-50%,-50%)",
+          width: "20%",
+        }}
+      >
+        <Button
+          variant="outlined"
+          onClick={handleClickOpen}
+          style={StartGameButton}
+        >
+          New Game
+        </Button>
+      </div>
+    </div>
+  );
 };
--- src/styles/ButtonStyles.ts
+++ src/styles/ButtonStyles.ts
@@ -4,3 +4,10 @@ export const PrimaryButtonStyle = {
   color: COLOR.FONT_SECONDARY,
   background: COLOR.BACKGROUND_PRIMARY,
 };
+
+export const StartGameButton = {
+  color: COLOR.FONT_SECONDARY,
+  borderColor: COLOR.FONT_SECONDARY,
+  width: "100%",
+  fontSize: "3.5vh",
+};
