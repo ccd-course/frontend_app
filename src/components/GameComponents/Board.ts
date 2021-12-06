1dbab939da76d667bb5ddf27604d3e0a70cf9e0c
majd-adawieh <majd.adawieh@icloud.com>
2021-12-06 18:18:53 +0100

    Draw a board on the canvas

--- src/components/GameComponents/Board.ts
+++ src/components/GameComponents/Board.ts
@@ -1,9 +1,15 @@
 import p5Types from "p5"; //Import this for typechecking and intellisense
 import { BoardTable } from "../../types";
+import { generateSquaresCoordinatesForOneCircle } from "./Helpers";
+import { Square } from "./Square";
 
 export class Board {
   private readonly numRows: number; // Number of rows on the baord
   private readonly numCols: number; // Number of cols on the board
+
+  // Object containing references to the generated squares (Key is the index of the square)
+  private squares: { [key: string]: Square } = {};
+  private boardCirclesRadious: number[]; // Needed to generate the squares
 
   constructor(
     private readonly p5Reference: p5Types,
@@ -12,6 +18,25 @@ export class Board {
     // Init the number of rows and cols
     this.numRows = this.boardTable[0].length;
     this.numCols = this.boardTable.length;
+
+    // Calculate the radiouses of the circles on the board
+    this.boardCirclesRadious = this.generateBoardCirclesRadiousList();
+
+    // Generate the list of squares and pieces
+    this.generateSquares();
+  }
+
+  // Render the board
+  public drawBoard() {
+    this.drawSquares();
+    return this;
+  }
+
+  // Render all the squares
+  private drawSquares() {
+    Object.keys(this.squares).forEach((key) => {
+      this.squares[key].drawSquare();
+    });
   }
 
   // Calculate the readious of board circles
@@ -24,5 +49,79 @@ export class Board {
       maxRadious -= distance;
     }
     return circlesRadiousList.reverse();
+  };
+
+  // Calculate the square coorindates
+  private generateSquares = () => {
+    const boardSquaresCoordinates = this.boardCirclesRadious.map(
+      (circleRadious: number) => {
+        return generateSquaresCoordinatesForOneCircle(
+          this.p5Reference,
+          circleRadious,
+          this.numCols
+        );
+      }
+    );
+
+    for (let i = 0; i < boardSquaresCoordinates.length - 1; i++) {
+      for (let j = 0; j < boardSquaresCoordinates[i].length - 1; j++) {
+        const square = new Square(
+          this.p5Reference,
+          {
+            p1: {
+              x: boardSquaresCoordinates[i][j].x,
+              y: boardSquaresCoordinates[i][j].y,
+            },
+            p2: {
+              x: boardSquaresCoordinates[i][j + 1].x,
+              y: boardSquaresCoordinates[i][j + 1].y,
+            },
+            p3: {
+              x: boardSquaresCoordinates[i + 1][j + 1].x,
+              y: boardSquaresCoordinates[i + 1][j + 1].y,
+            },
+            p4: {
+              x: boardSquaresCoordinates[i + 1][j].x,
+              y: boardSquaresCoordinates[i + 1][j].y,
+            },
+          },
+          [i + 1, boardSquaresCoordinates[i].length - (j + 1) + 1]
+        );
+        this.squares[
+          `{${i + 1},${boardSquaresCoordinates[i].length - (j + 1) + 1}}`
+        ] = square;
+      }
+    }
+    for (let i = 0; i < boardSquaresCoordinates.length - 1; i++) {
+      const square = new Square(
+        this.p5Reference,
+        {
+          p1: {
+            x: boardSquaresCoordinates[i][boardSquaresCoordinates[i].length - 1]
+              .x,
+            y: boardSquaresCoordinates[i][boardSquaresCoordinates[i].length - 1]
+              .y,
+          },
+          p2: {
+            x: boardSquaresCoordinates[i][0].x,
+            y: boardSquaresCoordinates[i][0].y,
+          },
+          p3: {
+            x: boardSquaresCoordinates[i + 1][0].x,
+            y: boardSquaresCoordinates[i + 1][0].y,
+          },
+          p4: {
+            x: boardSquaresCoordinates[i + 1][
+              boardSquaresCoordinates[i].length - 1
+            ].x,
+            y: boardSquaresCoordinates[i + 1][
+              boardSquaresCoordinates[i].length - 1
+            ].y,
+          },
+        },
+        [i + 1, 1]
+      );
+      this.squares[`{${i + 1},1}`] = square;
+    }
   };
 }
