c1921d1c9d80146814e37096675b43b6c2ed025b
majd-adawieh <majd.adawieh@icloud.com>
2021-12-06 18:10:19 +0100

    Init the square class

--- src/types.d.ts
+++ src/types.d.ts
@@ -9,3 +9,12 @@ export interface BoardPiece {
 }
 
 export type BoardTable = (BoardPiece | null)[][];
+
+export type Coordinate = { x: number; y: number };
+
+export interface SquareCoordinates {
+  p1: Coordinate;
+  p2: Coordinate;
+  p3: Coordinate;
+  p4: Coordinate;
+}
