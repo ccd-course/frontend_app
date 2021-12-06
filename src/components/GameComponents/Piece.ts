674a537200621fa99e460864e2faf79b3a834b66
majd-adawieh <majd.adawieh@icloud.com>
2021-12-06 18:42:27 +0100

    Add the needed attribute to the piece class

--- src/components/GameComponents/Piece.ts
+++ src/components/GameComponents/Piece.ts
@@ -1,1 +1,8 @@
-export class Piece {}
+import p5Types from "p5";
+
+export class Piece {
+  private pieceImage: Promise<p5Types.Image>; // Promise containing the image
+  private coordinate: Coordinate | undefined; // Postion of the piece on the canvas
+
+  private size: number | undefined; // Size of the piece
+}
