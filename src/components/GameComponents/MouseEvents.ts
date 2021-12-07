b285d6af13d2b942b514c95a11518c3da004ff9a
majd-adawieh <majd.adawieh@icloud.com>
2021-12-07 21:45:04 +0100

    Listen to events and publish them

--- src/components/GameComponents/MouseEvents.ts
+++ src/components/GameComponents/MouseEvents.ts
@@ -1,8 +1,24 @@
 import p5Types from "p5";
 import { Subject } from "rxjs";
+import { Coordinate } from "../../types";
 
 export const MouseEvent = new Subject<Coordinate>();
 
 export class MouseEvents {
   constructor(private canvas: p5Types.Element) {}
+
+  // Register the events
+  listen() {
+    this.onMouseClick();
+  }
+  // Listen to the mouse clicks on the canvas
+  // Send the values to the MouseEvents subject
+  private onMouseClick() {
+    this.canvas.mouseClicked((event: PointerEvent) => {
+      MouseEvent.next({
+        x: event.offsetX,
+        y: event.offsetY,
+      });
+    });
+  }
 }
