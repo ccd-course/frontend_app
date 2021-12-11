4b2878c5385353e081448f4287aa829ea5099222
majd-adawieh <majd.adawieh@icloud.com>
2021-12-11 11:51:43 +0100

    Add documentation to MouseEvents class and move the MouveEvent-storage

---
+++ src/storage/game_data.ts
@@ -0,0 +1,7 @@
+import { Subject } from "rxjs";
+import { Coordinate } from "../types";
+
+/**
+ * Different components can subscribe to this object and get the new data from it
+ */
+export const MouseEvent = new Subject<Coordinate>();
